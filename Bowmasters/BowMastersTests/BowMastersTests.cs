ecd6689a6870f3668b5673903129c15304e00d8d
okuma <m01@naudet.ch>
2025-01-21 09:13:44 +0100

    Test Unitaires
    Ajout d'un projet test unitaire avec 2 tests pour tester mes méthodes de balistic

---
+++ Bowmasters/BowMastersTests/BowMastersTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Bowmasters;
+
+namespace BowMastersTests
+{
+    [TestClass]
+    public class BowMastersTests
+    {
+        [TestMethod]
+        public void Balistic_OnYAxis_From_0_After5Sec_Result_51dot91()
+        {
+            // Arrange
+            int initialY = 0;
+            double time = 5;
+            double velocity = 20;
+            double angle = 0.785398;
+            double result;
+
+            // Act
+            result = Balistic.MovementOnYAxis(initialY, time, velocity, angle);
+
+            // Assert
+            Assert.AreEqual(51.91, Math.Round(result, 2), "Le résultat doit être 51.91");
+        }
+
+        [TestMethod]
+        public void Balistic_OnXAxist_From_0_After5Sec_Result_70dot71()
+        {
+            // Arrange
+            int initialX = 0;
+            double time = 5;
+            double velocity = 20;
+            double angle = 0.785398;
+            double result;
+
+            // Act
+            result = Balistic.MovementOnXAxis(initialX, time, velocity, angle);
+
+            // Assert
+            Assert.AreEqual(70.71, Math.Round(result, 2), "Le résultat doit être 70.71");
+        }
+    }
+}
