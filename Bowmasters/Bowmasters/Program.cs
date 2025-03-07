6be3d1400551cd31abb262a336f88ad5036dbc34
okuma <m01@naudet.ch>
2025-03-07 13:32:39 +0100

    Update(Constante): Mise a jour des constantes utilisées pour le jeu
    Fait en sorte que les joueurs et les tours se positionnent au meme endroit en fonction d'un endroit défini de base

--- Bowmasters/Bowmasters/Program.cs
+++ Bowmasters/Bowmasters/Program.cs
@@ -20,16 +20,16 @@ namespace Bowmasters
             const byte AMOUNT_OF_LIFE_PER_PLAYER = 3;                   // nombre de points de vie par joueur
             const byte PLAYER_NUMBER_FOR_PLAYER_ONE = 1;                // nombre du joueur 1
             const byte PLAYER_NUMBER_FOR_PLAYER_TWO = 2;                // nombre du joueur 2
-            const byte X_POSITION_PLAYER_1 = 20;                        // position x du joueur 1
-            const byte X_POSITION_PLAYER_2 = 127;                       // position x du joueur 2
-            const byte Y_POSITION_PLAYER_1_AND_2 = 37;                  // position y des joueurs 1 et 2
+            const byte X_POSITION_PLAYER_1 = 20;                                                    // position x du joueur 1
+            const byte X_POSITION_PLAYER_2 = Config.SCREEN_WIDTH - X_POSITION_PLAYER_1 - 3;                       // position x du joueur 2
+            const byte Y_POSITION_PLAYER_1_AND_2 = Config.SCREEN_HEIGHT - 3;                  // position y des joueurs 1 et 2
             const ConsoleColor COLOR_PLAYER_1 = ConsoleColor.Green;     // couleur du joueur 1
             const ConsoleColor COLOR_PLAYER_2 = ConsoleColor.Red;       // couleur du joueur 2
-            const byte TOWER_HEIGHT = 6;                                // hauteur des tours
-            const byte TOWER_WITDH = 3;                                 // largeur des tours
-            const byte X_POSITION_TOWER_ONE = 40;                       // position x de la tour du joueur 1
-            const byte X_POSITION_TOWER_TWO = 107;                      // position x de la tour du joueur 2
-            const byte Y_POSITION_TOWER_ONE_AND_TWO = 34;               // position y des tours du joueur 1 et 2
+            const byte TOWER_HEIGHT = 8;                                // hauteur des tours
+            const byte TOWER_WIDTH = 3;                                 // largeur des tours
+            const byte X_POSITION_TOWER_ONE = X_POSITION_PLAYER_1 + 13;                       // position x de la tour du joueur 1
+            const byte X_POSITION_TOWER_TWO = X_POSITION_PLAYER_2 - 8 - TOWER_WIDTH;                      // position x de la tour du joueur 2
+            const byte Y_POSITION_TOWER_ONE_AND_TWO = Config.SCREEN_HEIGHT - TOWER_HEIGHT;               // position y des tours du joueur 1 et 2
 
 
             // Déclaration et initialisation des variables ********************************************************************
@@ -38,8 +38,8 @@ namespace Bowmasters
             Player player1 = new Player(AMOUNT_OF_LIFE_PER_PLAYER, X_POSITION_PLAYER_1, Y_POSITION_PLAYER_1_AND_2, COLOR_PLAYER_1, PLAYER_NUMBER_FOR_PLAYER_ONE);    // joueur 1
             Player player2 = new Player(AMOUNT_OF_LIFE_PER_PLAYER, X_POSITION_PLAYER_2, Y_POSITION_PLAYER_1_AND_2, COLOR_PLAYER_2, PLAYER_NUMBER_FOR_PLAYER_TWO);    // joueur 2
             Towers towers;
-            Tower tower1 = new Tower(TOWER_HEIGHT, TOWER_WITDH, X_POSITION_TOWER_ONE, Y_POSITION_TOWER_ONE_AND_TWO); // tour du joueur 1
-            Tower tower2 = new Tower(TOWER_HEIGHT, TOWER_WITDH, X_POSITION_TOWER_TWO, Y_POSITION_TOWER_ONE_AND_TWO); // tour du joueur 2
+            Tower tower1 = new Tower(TOWER_HEIGHT, TOWER_WIDTH, X_POSITION_TOWER_ONE, Y_POSITION_TOWER_ONE_AND_TWO); // tour du joueur 1
+            Tower tower2 = new Tower(TOWER_HEIGHT, TOWER_WIDTH, X_POSITION_TOWER_TWO, Y_POSITION_TOWER_ONE_AND_TWO); // tour du joueur 2
 
 
             // Programme principal ***************************************************************************
