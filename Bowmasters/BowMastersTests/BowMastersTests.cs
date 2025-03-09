95f5056ee18ce3fdb4f17160749974cd6e3ebb63
okuma <m01@naudet.ch>
2025-03-09 10:26:23 +0100

    Update(testUnitaire): toujours des petits tweaks pour améliorer la présentation de mon code

--- Bowmasters/BowMastersTests/BowMastersTests.cs
+++ Bowmasters/BowMastersTests/BowMastersTests.cs
@@ -25,7 +25,7 @@ namespace BowMastersTests
             // Arrange
             int initialY = 0;
             double time = 5;
-            double velocity = 0;
+            double velocity = 20;
             double angle = 45;
             double result;
 
@@ -33,27 +33,27 @@ namespace BowMastersTests
             result = Balistic.MovementOnYAxis(initialY, time, velocity, angle);
 
             // Assert
-            Assert.AreEqual(122.625, result, "Le résultat doit être 122.625");
+            Assert.AreEqual(51.91, Math.Round(result, 2), "Le résultat doit être 51.91");
         }
 
         /// <summary>
         /// Vérifie que le mouvement de la balle sur l'axe X soit correct
         /// </summary>
         [TestMethod]
-        public void Balistic_OnXAxist_From_0_After5Sec_Result_70dot71()
+        public void Balistic_OnXAxist_From_0_After5Sec_Result_Minus70dot71()
         {
             // Arrange
             int initialX = 0;
             double time = 5;
             double velocity = 20;
-            double angle = 135;
+            double angle = 45;
             double result;
 
             // Act
             result = Balistic.MovementOnXAxis(initialX, time, velocity, angle);
 
             // Assert
-            Assert.AreEqual(-70.71, Math.Round(result, 2), "Le résultat doit être -70.71");
+            Assert.AreEqual(70.71, Math.Round(result, 2), "Le résultat doit être 70.71");
         }
     }
 }
