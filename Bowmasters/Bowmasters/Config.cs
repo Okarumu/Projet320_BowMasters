3e93813da483ff86bcf4b2fb98cad0c8b18cf18a
okuma <m01@naudet.ch>
2025-03-09 09:36:40 +0100

    update(paramètres du jeu): Les différents paramètres du jeu sont maintenant dans le fichier config.cs au lieu d'être dans le programme principal

--- Bowmasters/Bowmasters/Config.cs
+++ Bowmasters/Bowmasters/Config.cs
@@ -9,21 +9,91 @@ using System;
 namespace Bowmasters
 {
     /// <summary>
-    /// Permet de configurer la taille de la fenêtre de jeu
+    /// Permet de configurer les paramètres du jeu
+    /// Domaine d'application : La taille de la fenêtre, le nombre de point de vies des joueurs, leur positions ainsi que la taille et la position des tours
     /// </summary>
     static class Config
     {
         // Déclaration et initialisation des constantes ******************
 
+        // Paramètres pour la fenêtre
+
         /// <summary>
         /// Hauteur de la fenêtre
         /// </summary>
-        public const byte SCREEN_HEIGHT = 40;
+        private const byte _SCREEN_HEIGHT = 40;
 
         /// <summary>
         /// Largeur de la fenêtre
         /// </summary>
-        public const byte SCREEN_WIDTH = 150;
+        private const byte _SCREEN_WIDTH = 150;
+
+        // Paramètres du jeu
+
+        /// <summary>
+        /// nombre de points de vie par joueur
+        /// </summary>
+        public const byte AMOUNT_OF_LIFE_PER_PLAYER = 3;
+
+        /// <summary>
+        /// nombre du joueur 1
+        /// </summary>
+        public const byte PLAYER_NUMBER_FOR_PLAYER_ONE = 1;
+
+        /// <summary>
+        /// nombre du joueur 2
+        /// </summary>
+        public const byte PLAYER_NUMBER_FOR_PLAYER_TWO = 2;
+
+        /// <summary>
+        /// position x du joueur 1
+        /// </summary>
+        public const byte X_POSITION_PLAYER_1 = 20;
+
+        /// <summary>
+        /// position x du joueur 2
+        /// </summary>
+        public const byte X_POSITION_PLAYER_2 = _SCREEN_WIDTH - X_POSITION_PLAYER_1 - 3;
+
+        /// <summary>
+        /// position y des joueurs 1 et 2
+        /// </summary>
+        public const byte Y_POSITION_PLAYER_1_AND_2 = _SCREEN_HEIGHT - 3;
+
+        /// <summary>
+        /// couleur du joueur 1
+        /// </summary>
+        public const ConsoleColor COLOR_PLAYER_1 = ConsoleColor.Green;
+
+        /// <summary>
+        /// couleur du joueur 2
+        /// </summary>
+        public const ConsoleColor COLOR_PLAYER_2 = ConsoleColor.Red;
+
+        /// <summary>
+        /// hauteur des tours
+        /// </summary>
+        public const byte TOWER_HEIGHT = 8;
+
+        /// <summary>
+        /// largeur des tours
+        /// </summary>
+        public const byte TOWER_WIDTH = 3;
+
+        /// <summary>
+        /// position x de la tour du joueur 1
+        /// </summary>
+        public const byte X_POSITION_TOWER_ONE = X_POSITION_PLAYER_1 + 13;
+
+        /// <summary>
+        /// position x de la tour du joueur 2
+        /// </summary>
+        public const byte X_POSITION_TOWER_TWO = X_POSITION_PLAYER_2 - 8 - TOWER_WIDTH;
+
+        /// <summary>
+        /// position y des tours du joueur 1 et 2
+        /// </summary>
+        public const byte Y_POSITION_TOWER_ONE_AND_TWO = _SCREEN_HEIGHT - TOWER_HEIGHT;               
 
         // Déclaration et implémentation des méthodes ********************
 
@@ -32,8 +102,8 @@ namespace Bowmasters
         /// </summary>
         static public void SetGameOptions()
         {
-            Console.SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-            Console.SetBufferSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+            Console.SetWindowSize(_SCREEN_WIDTH, _SCREEN_HEIGHT);
+            Console.SetBufferSize(_SCREEN_WIDTH, _SCREEN_HEIGHT);
             Console.CursorVisible = false;
         }
     }
--- Bowmasters/Bowmasters/Program.cs
+++ Bowmasters/Bowmasters/Program.cs
@@ -15,22 +15,6 @@ namespace Bowmasters
     {
         static void Main()
         {
-            // Déclaration et initialisation des constantes *******************************************************************
-            const byte AMOUNT_OF_LIFE_PER_PLAYER = 3;                   // nombre de points de vie par joueur
-            const byte PLAYER_NUMBER_FOR_PLAYER_ONE = 1;                // nombre du joueur 1
-            const byte PLAYER_NUMBER_FOR_PLAYER_TWO = 2;                // nombre du joueur 2
-            const byte X_POSITION_PLAYER_1 = 20;                                                    // position x du joueur 1
-            const byte X_POSITION_PLAYER_2 = Config.SCREEN_WIDTH - X_POSITION_PLAYER_1 - 3;                       // position x du joueur 2
-            const byte Y_POSITION_PLAYER_1_AND_2 = Config.SCREEN_HEIGHT - 3;                  // position y des joueurs 1 et 2
-            const ConsoleColor COLOR_PLAYER_1 = ConsoleColor.Green;     // couleur du joueur 1
-            const ConsoleColor COLOR_PLAYER_2 = ConsoleColor.Red;       // couleur du joueur 2
-            const byte TOWER_HEIGHT = 8;                                // hauteur des tours
-            const byte TOWER_WIDTH = 3;                                 // largeur des tours
-            const byte X_POSITION_TOWER_ONE = X_POSITION_PLAYER_1 + 13;                       // position x de la tour du joueur 1
-            const byte X_POSITION_TOWER_TWO = X_POSITION_PLAYER_2 - 8 - TOWER_WIDTH;                      // position x de la tour du joueur 2
-            const byte Y_POSITION_TOWER_ONE_AND_TWO = Config.SCREEN_HEIGHT - TOWER_HEIGHT;               // position y des tours du joueur 1 et 2
-
-
             // Déclaration des variables ********************************************************************
 
             /// <summary>
@@ -73,13 +57,13 @@ namespace Bowmasters
 
             // Initialisation des variables utilisées dans le jeu ***********************
             // Initialisation des joueurs
-            player1 = new Player(AMOUNT_OF_LIFE_PER_PLAYER, X_POSITION_PLAYER_1, Y_POSITION_PLAYER_1_AND_2, COLOR_PLAYER_1, PLAYER_NUMBER_FOR_PLAYER_ONE);    
-            player2 = new Player(AMOUNT_OF_LIFE_PER_PLAYER, X_POSITION_PLAYER_2, Y_POSITION_PLAYER_1_AND_2, COLOR_PLAYER_2, PLAYER_NUMBER_FOR_PLAYER_TWO);
+            player1 = new Player(Config.AMOUNT_OF_LIFE_PER_PLAYER, Config.X_POSITION_PLAYER_1, Config.Y_POSITION_PLAYER_1_AND_2, Config.COLOR_PLAYER_1, Config.PLAYER_NUMBER_FOR_PLAYER_ONE);    
+            player2 = new Player(Config.AMOUNT_OF_LIFE_PER_PLAYER, Config.X_POSITION_PLAYER_2, Config.Y_POSITION_PLAYER_1_AND_2, Config.COLOR_PLAYER_2, Config.PLAYER_NUMBER_FOR_PLAYER_TWO);
             players = new Players(player1, player2);
 
             // Iniialisation des tours
-            tower1 = new Tower(TOWER_HEIGHT, TOWER_WIDTH, X_POSITION_TOWER_ONE, Y_POSITION_TOWER_ONE_AND_TWO); 
-            tower2 = new Tower(TOWER_HEIGHT, TOWER_WIDTH, X_POSITION_TOWER_TWO, Y_POSITION_TOWER_ONE_AND_TWO);             
+            tower1 = new Tower(Config.TOWER_HEIGHT, Config.TOWER_WIDTH, Config.X_POSITION_TOWER_ONE, Config.Y_POSITION_TOWER_ONE_AND_TWO); 
+            tower2 = new Tower(Config.TOWER_HEIGHT, Config.TOWER_WIDTH, Config.X_POSITION_TOWER_TWO, Config.Y_POSITION_TOWER_ONE_AND_TWO);             
             towers = new Towers(tower1, tower2); 
             
             // Initialisation du jeu en lui-même
